cef4b0ef9c4fdcf5ab272b8db4d59cdc8857fd3d
etim david <etimdavid400@gmail.com>
2025-10-30 13:56:50 -0700

    Add full project files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
-import ProfilePic from "../assets/profile.jpg";
+import ProfilePic from "../assets/profile1.jpg";
 
 export default function Home() {
   useEffect(() => {
